c4f5cf3410f247e802fe8c5588a3d30cf4be215a
Eamonn de Leastar <edeleastar@wit.ie>
2023-01-26 18:15:53 +0000

    first part of 0.3.0

---
+++ donation-svelte-0.3.0/src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { goto } from "$app/navigation";
+	import { onMount } from "svelte";
+	import { donationService } from "../services/donation-service";
+	import { user } from "../stores";
+
+	onMount(async () => {
+		donationService.reload();
+	});
+</script>
+
+<div class="container">
+	<slot />
+</div>
