1df3bfbb3640bb6dfe1ded7f308a38d0fba48dd7
Eamonn de Leastar <edeleastar@wit.ie>
2023-01-29 18:12:19 +0000

    donation 0.4.0 finalised

---
+++ donation-svelte-0.4.0/src/routes/donate/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+	import Header from "$lib/Header.svelte";
+	import DonateForm from "$lib/DonateForm.svelte";
+	import MainNavigator from "$lib/MainNavigator.svelte";
+	import { beforeUpdate } from "svelte";
+	import { donationService } from "../../services/donation-service";
+	import DonationMap from "$lib/DonationMap.svelte";
+
+	beforeUpdate(() => {
+		donationService.checkPageRefresh();
+	});
+</script>
+
+<Header>
+	<MainNavigator />
+</Header>
+
+<div class="columns is-vcentered">
+	<div class="column has-text-centered">
+		<DonationMap />
+	</div>
+	<div class="column box has-text-centered">
+		<h1 class="title is-4">Give Generously!</h1>
+		<DonateForm />
+	</div>
+</div>
