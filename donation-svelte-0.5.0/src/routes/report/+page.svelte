3601bdac3d75bb5730f59a38ecb58ead345dfe2c
Eamonn de Leastar <edeleastar@wit.ie>
2023-01-30 08:32:44 +0000

    first version of 0.5.0

---
+++ donation-svelte-0.5.0/src/routes/report/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+	import Header from "$lib/Header.svelte";
+	import MainNavigator from "$lib/MainNavigator.svelte";
+	import DonationList from "$lib/DonationList.svelte";
+	import { beforeUpdate } from "svelte";
+	import { donationService } from "../../services/donation-service";
+
+	beforeUpdate(() => {
+		donationService.checkPageRefresh();
+	});
+</script>
+
+<Header>
+	<MainNavigator />
+</Header>
+
+<div class="columns">
+	<div class="column has-text-centered">
+		<img alt="Homer" src="/homer4.jpeg" width="300" />
+	</div>
+	<div class="column box has-text-centered">
+		<h1 class="title is-4">Donations to date</h1>
+		<DonationList />
+	</div>
+</div>
